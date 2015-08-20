d55a7822fb23cde6197d52e6429fc373c1b83fb8
unknown <D:\Programs\Network\The Bat!\Mail>
2015-08-20 18:10:31 +0300

    Initial commit

---
+++ Sources/TagzLib/Format.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Antlr4.Runtime;
+using Antlr4.Runtime.Tree;
+
+namespace Tagz
+{
+	public class Format
+	{
+		private class TagzKeyPrinter : TagzBaseListener
+		{
+			// override default listener behavior
+			public override void EnterEveryRule(ParserRuleContext context)
+			{
+				base.EnterEveryRule(context);
+				Console.WriteLine("rule: " + context.RuleIndex);
+			}
+
+			public override void VisitTerminal(ITerminalNode node)
+			{
+				base.VisitTerminal(node);
+				Console.WriteLine("term: " + node.ToString());
+			}
+		}
+
+		public void TestGrammar()
+		{
+			String input = "hello zome\r\nhello tezt";
+			AntlrInputStream stream = new AntlrInputStream(input);
+			ITokenSource lexer = new TagzLexer(stream);
+			ITokenStream tokens = new CommonTokenStream(lexer);
+			TagzParser parser = new TagzParser(tokens);
+			parser.BuildParseTree = true;
+			IParseTree tree = parser.r();
+			TagzKeyPrinter printer = new TagzKeyPrinter();
+			ParseTreeWalker.Default.Walk(printer, tree);
+		}
+
+		public string Do(string template)
+		{
+			return "==" + template + "==";
+		}
+	}
+}
---
+++ Sources/TestApp/MainForm.Designer.cs
@@ -0,0 +1,88 @@
+﻿namespace TestApp
+{
+	partial class MainForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.textTemplate = new System.Windows.Forms.TextBox();
+			this.textResult = new System.Windows.Forms.TextBox();
+			this.button1 = new System.Windows.Forms.Button();
+			this.SuspendLayout();
+			// 
+			// textTemplate
+			// 
+			this.textTemplate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textTemplate.Location = new System.Drawing.Point(12, 12);
+			this.textTemplate.Name = "textTemplate";
+			this.textTemplate.Size = new System.Drawing.Size(630, 20);
+			this.textTemplate.TabIndex = 0;
+			// 
+			// textResult
+			// 
+			this.textResult.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+			this.textResult.Location = new System.Drawing.Point(12, 38);
+			this.textResult.Name = "textResult";
+			this.textResult.ReadOnly = true;
+			this.textResult.Size = new System.Drawing.Size(630, 20);
+			this.textResult.TabIndex = 1;
+			// 
+			// button1
+			// 
+			this.button1.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+			this.button1.Location = new System.Drawing.Point(567, 64);
+			this.button1.Name = "button1";
+			this.button1.Size = new System.Drawing.Size(75, 23);
+			this.button1.TabIndex = 2;
+			this.button1.Text = "button1";
+			this.button1.UseVisualStyleBackColor = true;
+			this.button1.Click += new System.EventHandler(this.button1_Click);
+			// 
+			// MainForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(654, 273);
+			this.Controls.Add(this.button1);
+			this.Controls.Add(this.textResult);
+			this.Controls.Add(this.textTemplate);
+			this.Name = "MainForm";
+			this.Text = "Tagz Test App";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.TextBox textTemplate;
+		private System.Windows.Forms.TextBox textResult;
+		private System.Windows.Forms.Button button1;
+	}
+}
+
